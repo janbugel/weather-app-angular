b74b92696866ad29493d9111158c16261b03b2f6
janbugel <janbugel11@gmail.com>
2024-03-12 09:26:22 +0100

    minor updates

--- src/app/components/weather-table/weather-table.component.ts
+++ src/app/components/weather-table/weather-table.component.ts
@@ -6,6 +6,7 @@ interface WeatherData {
   temperature: number;
   humidity: number;
   pressure: number | string;
+  weatherState: string;
 }
 
 @Component({
@@ -16,6 +17,7 @@ export class WeatherTableComponent implements OnInit {
   weatherData: WeatherData[] = [];
   displayedColumns: string[] = [
     'datetime',
+    'weatherState',
     'temperature',
     'humidity',
     'pressure',
@@ -44,8 +46,43 @@ export class WeatherTableComponent implements OnInit {
           temperature: data.hourly.temperature_2m[index],
           humidity: data.hourly.relative_humidity_2m[index],
           pressure: data.hourly.pressure_msl[index],
+          weatherState: this.mapWeatherCodeToState(data.hourly.weather_code[index]),
         };
       });
     });
+  }
+
+  private mapWeatherCodeToState(code: number): string {
+    switch (code) {
+      case 0: return 'Clear sky';
+      case 1: return 'Mainly clear';
+      case 2: return 'Partly cloudy';
+      case 3: return 'Cloudy';
+      case 45: return 'Fog';
+      case 48: return 'Depositing rime fog';
+      case 51: return 'Light drizzle';
+      case 53: return 'Moderate drizzle';
+      case 55: return 'Dense drizzle';
+      case 56: return 'Light freezing Drizzle';
+      case 57: return 'Dense freezing Drizzle';
+      case 61: return 'Slight rain';
+      case 63: return 'Moderate rain';
+      case 65: return 'Heavy rain';
+      case 66: return 'Light freezing rain';
+      case 67: return 'Heavy freezing rain';
+      case 71: return 'Slight snow fall';
+      case 73: return 'Moderate snow fall';
+      case 75: return 'Heavy snow fall';
+      case 77: return 'Snow Grains';
+      case 80: return 'Slight rain showers';
+      case 81: return 'Moderate rain showers';
+      case 82: return 'Heavy rain showers';
+      case 85: return 'Slight snow showers';
+      case 86: return 'Heavy snow showers';
+      case 95: return 'Thunderstorm: Slight or moderate';
+      case 96: return 'Thunderstorm with light hail';
+      case 99: return 'Thunderstorm with heavy hail';
+      default: return 'Unknown';
+    }
   }
 }
--- src/app/services/forecast.service.ts
+++ src/app/services/forecast.service.ts
@@ -14,7 +14,7 @@ export class ForecastService {
     const params = {
       latitude: 51.5074, // London's latitude
       longitude: -0.1278, // London's longitude
-      hourly: 'temperature_2m,relative_humidity_2m,pressure_msl',
+      hourly: 'temperature_2m,relative_humidity_2m,pressure_msl,weather_code',
       past_days: 7
     };
     return this.http.get(this.baseUrl, { params });
