00e8aa8a88c8df8ed301047727caa2cbdeb474c7
janbugel <janbugel11@gmail.com>
2024-03-13 23:28:25 +0100

    started refactoring

--- src/app/components/weather-table/weather-table.component.ts
+++ src/app/components/weather-table/weather-table.component.ts
@@ -80,50 +80,11 @@ export class WeatherTableComponent implements OnInit, AfterViewInit {
           temperature: data.hourly.temperature_2m[index],
           humidity: data.hourly.relative_humidity_2m[index],
           pressure: data.hourly.pressure_msl[index],
-          weatherState: this.mapWeatherCodeToState(data.hourly.weather_code[index]),
+          weatherState: this.forecastService.mapWeatherCodeToState(data.hourly.weather_code[index]),
         };
       });
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
-  }
-
-  private mapWeatherCodeToState(code: number): string {
-    switch (code) {
-      case 0: return 'Clear sky';
-      case 1: return 'Mainly clear';
-      case 2: return 'Partly cloudy';
-      case 3: return 'Cloudy';
-      case 45: return 'Fog';
-      case 48: return 'Depositing rime fog';
-      case 51: return 'Light drizzle';
-      case 53: return 'Moderate drizzle';
-      case 55: return 'Dense drizzle';
-      case 56: return 'Light freezing drizzle';
-      case 57: return 'Dense freezing drizzle';
-      case 61: return 'Slight rain';
-      case 63: return 'Moderate rain';
-      case 65: return 'Heavy rain';
-      case 66: return 'Light freezing rain';
-      case 67: return 'Heavy freezing rain';
-      case 71: return 'Slight snow fall';
-      case 73: return 'Moderate snow fall';
-      case 75: return 'Heavy snow fall';
-      case 77: return 'Snow grains';
-      case 80: return 'Slight rain showers';
-      case 81: return 'Moderate rain showers';
-      case 82: return 'Heavy rain showers';
-      case 85: return 'Slight snow showers';
-      case 86: return 'Heavy snow showers';
-      case 95: return 'Thunderstorm: Slight or moderate';
-      case 96: return 'Thunderstorm with light hail';
-      case 99: return 'Thunderstorm with heavy hail';
-      default: return 'Unknown';
-    }
-  }
-
-  updatePaginatorSettings(event: PageEvent) {
-    localStorage.setItem('paginatorPageIndex', event.pageIndex.toString());
-    localStorage.setItem('paginatorPageSize', event.pageSize.toString());
   }
 }
