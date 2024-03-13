b447e2d4515ffa4561b54c299bf76d6623ec824f
janbugel <janbugel11@gmail.com>
2024-03-13 11:58:11 +0100

    small fix

--- docs/chunk-PYZCTDLT.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-HJYVVYYS.js";import"./chunk-VP4LWBQ2.js";import"./chunk-7EPBRA3T.js";import"./chunk-ZTI4EJEI.js";export{a as WeatherTableModule};
---
+++ docs/chunk-UTM4KQQJ.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-3QM4AOW2.js";import"./chunk-VP4LWBQ2.js";import"./chunk-7EPBRA3T.js";import"./chunk-ZTI4EJEI.js";export{a as WeatherTableModule};
